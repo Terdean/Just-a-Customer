86c791d3a2c83fad58a5ec24ed66179ad4a242e0
Terdean146 <terdean146@gmail.com>
2023-07-21 13:23:09 +0200

    PuckUp 0.2

--- Just a Customer/Assets/Scripts/Item PickUp MimiGame/ItemPickUp.cs
+++ Just a Customer/Assets/Scripts/Item PickUp MimiGame/ItemPickUp.cs
@@ -7,15 +7,18 @@ public class ItemPickUp : MonoBehaviour
 {
     private Vector3 dragOffset;
     public ItemPickUpWalls walls;
+    public GameObject center;
 
     private void OnMouseDown()
     {
-        if(!walls.isObjectTouchedWall) dragOffset = this.transform.position - GetMousePosition();
+        if (!walls.isObjectTouchedWall) dragOffset = this.transform.position - GetMousePosition();
+        else if (walls.isObjectTouchedWall) transform.position = Vector3.MoveTowards(transform.position, center.transform.position, 1f);
     }
 
     private void OnMouseDrag()
     {
         if (!walls.isObjectTouchedWall) this.transform.position = GetMousePosition() + dragOffset;
+        else if (walls.isObjectTouchedWall) transform.position = Vector3.MoveTowards(transform.position, center.transform.position, 1f);
     }
 
     private Vector3 GetMousePosition()
