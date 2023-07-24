77ab8275b92fae44fac1b541f7f5366281423508
Terdean146 <terdean146@gmail.com>
2023-07-24 21:09:23 +0200

    Breathing 0.1 + Blinking 0.2
    Дыхание!
    Кучу анимаций с собой это понесло. По сути я буквально умножил на 4 все анимашки Вадима и не забыл пофиксить багушки-бугагагашеньки. Скрипт работает почти как скрипт моргания + теперь появляется синяя виньетка, когда игрок задыхается

--- Just a Customer/Assets/Scripts/QTE/Breath.cs
+++ Just a Customer/Assets/Scripts/QTE/Breath.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.UI;
 
-public class Blinking : MonoBehaviour
+public class Breath : MonoBehaviour
 {
-    public float blinkLevel; //Можно в инспекторе будет менять для каждого отдельного уровня, чтобы игрок мог сразу начать без глаз О-О
-    public float blinkLevelDecreaser; //На это значение раз в какое-то время будет уменьшаться уровень моргания
-    public float blinkLevelIncreser; //На сколько увеличивается шкала моргания, если нажать на нужную клавишу
-    private float blinkTimer;
-    public float blinkTimerStart; //Время раз в которое будет уменьшаться уровень моргания
+    public float breathLevel; //Можно в инспекторе будет менять для каждого отдельного уровня, чтобы игрок мог сразу начать без глаз О-О
+    public float breathLevelDecreaser; //На это значение раз в какое-то время будет уменьшаться уровень моргания
+    public float breathLevelIncreser; //На сколько увеличивается шкала моргания, если нажать на нужную клавишу
+    private float breathTimer;
+    public float breathTimerStart; //Время раз в которое будет уменьшаться уровень моргания
 
     public Subsequence sucub; //Сюда надо добавить тот объект со скриптом Subsequence, который будет отвечать за нужную для нажатия кнопку
     public Text textSign; //Текст, что будет обозначать в интерфейсе нужную для нажатия клавишу
@@ -20,11 +20,11 @@ public class Blinking : MonoBehaviour
     [HideInInspector]
     public ImportantKeysManager importantKeysManager;
 
-    private DepthOfField blur;
+    private Vignette vinnipuh;
     [SerializeField]
     private PostProcessVolume volume; //Перетаскиваешь сюды камеру и всё работает
-    private float valueMax = 50;
-    private float valueCurrent = 50;
+    private float valueMax = 0;
+    private float valueCurrent = 0;
     public health PlayerHealth;
 
     private int _foreachNum;
@@ -32,7 +32,7 @@ public class Blinking : MonoBehaviour
     private void Start()
     {
         importantKeysManager = GameObject.Find("ImportantKeysManager").GetComponent<ImportantKeysManager>();
-        volume.profile.TryGetSettings(out blur);
+        volume.profile.TryGetSettings(out vinnipuh);
     }
 
     void Update()
@@ -52,55 +52,61 @@ public class Blinking : MonoBehaviour
             if (importantKeysManager.isSameLatterFound == false) sucubTimer = sucubTimerStart;
         }
 
-        if (blinkTimer > 0 && blinkLevel >= -100) blinkTimer -= Time.deltaTime;
-        else if (blinkTimer <= 0 && blinkLevel >= -100)
+        if (breathTimer > 0 && breathLevel >= -100) breathTimer -= Time.deltaTime;
+        else if (breathTimer <= 0 && breathLevel >= -100)
         {
-            blinkLevel -= blinkLevelDecreaser;
-            blinkTimer = blinkTimerStart;
+            breathLevel -= breathLevelDecreaser;
+            breathTimer = breathTimerStart;
         }
 
-        if (valueCurrent < valueMax) valueCurrent += Time.deltaTime * 3;
-        if (valueCurrent > valueMax) valueCurrent -= Time.deltaTime * 3;
-        
-        if (blinkLevel > -20 && blinkLevel < 70)
+        if (valueCurrent < valueMax) valueCurrent += Time.deltaTime * 0.01f;
+        if (valueCurrent > valueMax) valueCurrent -= Time.deltaTime * 0.01f;
+
+        if (breathLevel > -20 && breathLevel < 70)
         {
-            valueMax = 50;
-            blur.focalLength.value = valueCurrent;
+            valueMax = 0f;
+            vinnipuh.intensity.value = valueCurrent;
         }
-        if (blinkLevel <= -20)
+        if (breathLevel <= -20)
         {
-            valueMax = 100;
-            if (blinkLevel <= -50)
+            valueMax = 0.11f;
+            if (breathLevel > -50) PlayerHealth.Malevich.SetInteger("BreathLevel", 1);
+            if (breathLevel <= -50)
             {
-                valueMax = 200;
+                valueMax = 0.22f;
                 PlayerHealth.hp_minus(Time.deltaTime);
+                PlayerHealth.Malevich.SetInteger("BreathLevel", 2);
             }
-            if (blinkLevel <= -80)
+            if (breathLevel <= -80)
             {
-                valueMax = 300;
+                valueMax = 0.33f;
                 PlayerHealth.hp_minus(Time.deltaTime * 2);
+                PlayerHealth.Malevich.SetInteger("BreathLevel", 3);
             }
-            blur.focalLength.value = valueCurrent;
+            vinnipuh.intensity.value = valueCurrent;
         }
-        if (blinkLevel >= 70)
+        if (breathLevel >= 70)
         {
-            valueMax = 100;
-            if (blinkLevel >= 80)
+            valueMax = 0.11f;
+            if (breathLevel < 80) PlayerHealth.Malevich.SetInteger("BreathLevel", 1);
+            if (breathLevel >= 80)
             {
-                valueMax = 200;
+                valueMax = 0.22f;
                 PlayerHealth.hp_minus(Time.deltaTime);
+                PlayerHealth.Malevich.SetInteger("BreathLevel", 2);
             }
-            if (blinkLevel >= 90)
+            if (breathLevel >= 90)
             {
-                valueMax = 300;
+                valueMax = 0.33f;
                 PlayerHealth.hp_minus(Time.deltaTime * 2);
+                PlayerHealth.Malevich.SetInteger("BreathLevel", 3);
             }
-            blur.focalLength.value = valueCurrent;
+            vinnipuh.intensity.value = valueCurrent;
         }
 
         if (sucub.isEverySequencesTrue)
         {
-            blinkLevel += blinkLevelIncreser;
+            breathLevel += breathLevelIncreser;
 
             sucub.isEverySequencesTrue = false;
             foreach (bool i in sucub.sequences)
@@ -112,6 +118,6 @@ public class Blinking : MonoBehaviour
             sucub.latterNumber = 0;
         }
 
-        textSign.text = sucub.latters[0] + " - моргание";
+        textSign.text = sucub.latters[0] + " - вдох";
     }
 }
